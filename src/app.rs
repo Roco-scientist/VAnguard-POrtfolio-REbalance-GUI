1423db82f0ba309e626d326ccbef92597d277783
Rory Coffey <coffeyrt@gmail.com>
2024-04-24 20:54:47 -0400

    annotation added

--- src/app.rs
+++ src/app.rs
@@ -14,59 +14,61 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct VaporeApp {
-    profile_name: String,
-    birth_year: HashMap<String, u32>,
-    retirement_year: HashMap<String, i32>,
-    brokerage_stock: HashMap<String, u32>,
-    brokerage_account_num: HashMap<String, u32>,
-    roth_account_num: HashMap<String, u32>,
-    trad_account_num: HashMap<String, u32>,
-    distribution_table: HashMap<u32, f32>,
+    profile_name: String, // Holds the profile name that is used to retrieve birth year etc
+    birth_year: HashMap<String, u32>, // Profile name: year
+    retirement_year: HashMap<String, i32>, // Profile name: year
+    brokerage_stock: HashMap<String, u32>, // Profile name: brokerage stock percent
+    brokerage_account_num: HashMap<String, u32>, // Profile name: brokerage account number
+    roth_account_num: HashMap<String, u32>, // Profile name: Roth account number
+    trad_account_num: HashMap<String, u32>, // Profile name: Traditional IRA account number
+    distribution_table: HashMap<u32, f32>, // Age: divider from IRS' distribution table
     #[serde(skip)]
-    distribution_year: u32,
+    distribution_year: u32, // Year for which distributions are calculated
     #[serde(skip)]
-    brokerage_cash_add: i32,
+    brokerage_cash_add: i32, // Amount of brokerage cahs added
     #[serde(skip)]
-    brokerage_us_stock_add: f32,
+    brokerage_us_stock_add: f32, // US stock add to brokerage, e.g. Alpaca or manually added
     #[serde(skip)]
-    brokerage_int_stock_add: f32,
+    brokerage_int_stock_add: f32, // Stock add unused at this time
     #[serde(skip)]
-    brokerage_us_bond_add: f32,
+    brokerage_us_bond_add: f32, // Bond add unused at this time
     #[serde(skip)]
-    brokerage_int_bond_add: f32,
+    brokerage_int_bond_add: f32, // Bond add unused at this time
     #[serde(skip)]
-    roth_holdings: ShareValues,
+    roth_holdings: ShareValues, // Roth value holdings found from account number
     #[serde(skip)]
-    roth_us_stock_add: f32,
+    roth_us_stock_add: f32, // Stock add unused at this time
     #[serde(skip)]
-    roth_us_bond_add: f32,
+    roth_us_bond_add: f32, // Bond add unused at this time
     #[serde(skip)]
-    roth_int_stock_add: f32,
+    roth_int_stock_add: f32, // Stock add unused at this time
     #[serde(skip)]
-    roth_int_bond_add: f32,
+    roth_int_bond_add: f32, // Bond add unused at this time
     #[serde(skip)]
-    roth_cash_add: i32,
+    roth_cash_add: i32, // Cash added or subtracted from Roth account
     #[serde(skip)]
-    traditional_holdings: ShareValues,
+    traditional_holdings: ShareValues, // Traditional stock values found with the account number
     #[serde(skip)]
-    traditional_us_stock_add: f32,
+    traditional_us_stock_add: f32, // Stock add unused at this time
     #[serde(skip)]
-    traditional_us_bond_add: f32,
+    traditional_us_bond_add: f32, // Bond add unused at this time
     #[serde(skip)]
-    traditional_int_stock_add: f32,
+    traditional_int_stock_add: f32, // Stock add unused at this time
     #[serde(skip)]
-    traditional_int_bond_add: f32,
+    traditional_int_bond_add: f32, // Bond add unused at this time
     #[serde(skip)]
-    traditional_cash_add: i32,
-    use_brokerage_retirement: bool,
+    traditional_cash_add: i32, // Cash to add or subtract from the Traditional IRA
+    use_brokerage_retirement: bool, // Whether to use the brokerage as the same allocation as retirement
     #[serde(skip)]
-    brokerage_holdings: ShareValues,
+    brokerage_holdings: ShareValues, // Brokerage holdings found from the account number
     #[serde(skip)]
-    rebalance: VanguardRebalance,
+    rebalance: VanguardRebalance, // Targets and purchases/sales needed to rebalance
     #[serde(skip)]
     vanguard_holdings: Arc<Mutex<VanguardHoldings>>,
 }
@@ -109,13 +111,9 @@ impl Default for VaporeApp {
 }
 
 impl VaporeApp {
-    /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
+        // Load previous app state
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
@@ -132,14 +130,11 @@ impl eframe::App for VaporeApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
 
+            // Menu bar if not WASM website
             egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
                 if !is_web {
                     ui.menu_button("File", |ui| {
@@ -155,22 +150,24 @@ impl eframe::App for VaporeApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
             ui.heading("VAnguard POrtfolio REbalance");
 
+            // Button to import the Vanguard ofxdownload.csv file
             if ui.button("Open Vanguard File").clicked() {
                 let file_future = rfd::AsyncFileDialog::new().pick_file();
                 let vanguard_holdings = Arc::clone(&self.vanguard_holdings);
+                // Function below used to import and be compatable with both OS and WASM
                 execute(async move {
                     if let Some(file) = file_future.await {
                         *vanguard_holdings.lock().unwrap() =
                             parse_csv_download(String::from_utf8(file.read().await).unwrap())
                                 .await
                                 .unwrap();
-                        // parse_csv_download(String::from_utf8(file.read().await).unwrap()).await.unwrap();
                     }
                 });
             };
+
+            // Update with Yahoo quotes which is only possible when not WASM/website
             #[cfg(not(target_arch = "wasm32"))]
             ui.horizontal(|ui| {
                 if ui.button("Update with Yahoo stock quotes").clicked() {
@@ -179,7 +176,11 @@ impl eframe::App for VaporeApp {
                 };
             });
 
+            // Profile creator with all values that are specific to each profile, such as account
+            // numbers, birth year, and retirement year
             egui::CollapsingHeader::new("Profile").show(ui, |ui| {
+
+                // Selector for any previously made profiles
                 egui::ComboBox::from_id_source("Brokerage")
                     .selected_text(&self.profile_name)
                     .show_ui(ui, |ui| {
@@ -187,8 +188,11 @@ impl eframe::App for VaporeApp {
                             ui.selectable_value(&mut self.profile_name, profile.clone(), profile);
                         }
                     });
+
+                // Create or delete profile on a single horizontal frame
                 ui.horizontal(|ui| {
                     ui.add(egui::TextEdit::singleline(&mut self.profile_name));
+                    // Create new profile with the name from the text edit
                     if ui.button("Create").clicked() {
                         if !self.birth_year.contains_key(&self.profile_name) {
                             self.birth_year.insert(self.profile_name.clone(), 1980);
@@ -210,6 +214,8 @@ impl eframe::App for VaporeApp {
                             self.trad_account_num.insert(self.profile_name.clone(), 0);
                         };
                     };
+                    // Delete profile with the name from the text edit.  Remove all profile name
+                    // references in the profile HashMaps
                     if ui.button("Delete").clicked() {
                         self.birth_year.remove(&self.profile_name);
                         self.retirement_year.remove(&self.profile_name);
@@ -220,10 +226,12 @@ impl eframe::App for VaporeApp {
                     }
                 });
 
+                // Birth year add
                 if let Some(birth_year) = self.birth_year.get_mut(&self.profile_name) {
                     ui.add(egui::Slider::new(&mut *birth_year, 1940..=2100).text("Birth year"));
                 }
 
+                // Retirement year add
                 if let Some(retirement_year) = self.retirement_year.get_mut(&self.profile_name) {
                     ui.add(
                         egui::Slider::new(&mut *retirement_year, 2020..=2100)
@@ -231,6 +239,8 @@ impl eframe::App for VaporeApp {
                     );
                 };
 
+                // If a profile has been created, allow selection of brokerage account numbers derived from
+                // the Vanguard download file
                 if let Some(profile_account_num) =
                     self.brokerage_account_num.get_mut(&self.profile_name)
                 {
@@ -256,6 +266,9 @@ impl eframe::App for VaporeApp {
                         ui.checkbox(&mut self.use_brokerage_retirement, "Retirement");
                     });
                 };
+
+                // If a profile has been created, allow selection of IRA account numbers derived from
+                // the Vanguard download file
                 if let Some(profile_account_num) = self.trad_account_num.get_mut(&self.profile_name)
                 {
                     ui.horizontal(|ui| {
@@ -279,6 +292,9 @@ impl eframe::App for VaporeApp {
                             });
                     });
                 };
+
+                // If a profile has been created, allow selection of Roth IRA account numbers derived from
+                // the Vanguard download file
                 if let Some(profile_account_num) = self.roth_account_num.get_mut(&self.profile_name)
                 {
                     ui.horizontal(|ui| {
@@ -304,6 +320,8 @@ impl eframe::App for VaporeApp {
                 };
             });
 
+            // If there is a profile created, set the brokerage hodlings to the account number
+            // selected
             if let Some(brokerage_account_num) = self.brokerage_account_num.get(&self.profile_name)
             {
                 self.brokerage_holdings = self
@@ -315,6 +333,9 @@ impl eframe::App for VaporeApp {
                     .unwrap_or(&ShareValues::default())
                     .clone();
             };
+
+            // If there is a profile created, set the IRA hodlings to the account number
+            // selected
             if let Some(trad_account_num) = self.trad_account_num.get(&self.profile_name) {
                 self.traditional_holdings = self
                     .vanguard_holdings
@@ -325,6 +346,9 @@ impl eframe::App for VaporeApp {
                     .unwrap_or(&ShareValues::default())
                     .clone();
             };
+
+            // If there is a profile created, set the Roth IRA hodlings to the account number
+            // selected
             if let Some(roth_account_num) = self.roth_account_num.get(&self.profile_name) {
                 self.roth_holdings = self
                     .vanguard_holdings
@@ -336,6 +360,8 @@ impl eframe::App for VaporeApp {
                     .clone();
             };
 
+            // If brokerage percentage is not set by retirement ratios and is kept separate, create
+            // a slider to input brokerage stock percent
             if !self.use_brokerage_retirement {
                 if let Some(brokerage_stock) = self.brokerage_stock.get_mut(&self.profile_name) {
                     ui.add(
@@ -345,11 +371,13 @@ impl eframe::App for VaporeApp {
                 };
             };
 
+            // Add outside US stock value to the brokerage account calculations
             ui.horizontal(|ui| {
                 ui.add(
                     egui::Slider::new(&mut self.brokerage_us_stock_add, 0.0..=10000000.00)
                         .text("US stock value outside Vanguard"),
                 );
+                // If there is an Alpaca brokerage account, add that value
                 #[cfg(not(target_arch = "wasm32"))]
                 if ui.button("Add Alpaca").clicked() {
                     let key_id = std::env::var("APCA_API_KEY_ID").unwrap_or_else(|_| String::new());
@@ -372,21 +400,27 @@ impl eframe::App for VaporeApp {
                 };
             });
 
+            // Cash to add or subtract from the brokerage account
             ui.add(
                 egui::Slider::new(&mut self.brokerage_cash_add, -100000..=100000)
                     .text("Brokerage cash add/remove"),
             );
 
+            // Cash to add or subtract from the Roth IRA account
             ui.add(
                 egui::Slider::new(&mut self.roth_cash_add, -100000..=100000)
                     .text("Roth IRA cash add/remove"),
             );
 
+            // Cash to add or subtract from the IRA account
             ui.add(
                 egui::Slider::new(&mut self.traditional_cash_add, -100000..=100000)
                     .text("Traditional IRA cash add/remove"),
             );
 
+            // Distribution requirements after retirement age for the IRA.  Cannot be used by
+            // WASM/website due to needing to get Yahoo quotes to determine the previous end of 
+            // year account value
             #[cfg(not(target_arch = "wasm32"))]
             ui.horizontal(|ui| {
                 if ui.button("Load distribution table").clicked() {
@@ -395,6 +429,7 @@ impl eframe::App for VaporeApp {
                     };
                 };
 
+                // Selection for current or previous year to calculated needed distributions
                 let current_year = Local::now().year() as u32;
                 let last_year = current_year - 1;
                 ui.label("Distribution year:");
@@ -408,6 +443,8 @@ impl eframe::App for VaporeApp {
                     current_year,
                     current_year.to_string(),
                 );
+
+                // If the profile's age is old enough, display the distribution needed
                 if let Some(birth_year) = self.birth_year.get(&self.profile_name) {
                     if let Some(trad_account_num) = self.trad_account_num.get(&self.profile_name) {
                         let age = self.distribution_year - birth_year;
@@ -446,6 +483,7 @@ impl eframe::App for VaporeApp {
                 };
             });
 
+            // Update the purchase/sales needed to rebalance the portfolio
             if let Some(brokerage_stock) = self.brokerage_stock.get(&self.profile_name) {
                 if let Some(retirement_year) = self.retirement_year.get(&self.profile_name) {
                     if ui.button("Update").clicked() {
@@ -478,6 +516,7 @@ impl eframe::App for VaporeApp {
                 }
             }
 
+            // Display the update hodlings within a drop menu
             egui::CollapsingHeader::new("Holdings").show(ui, |ui| {
                 ui.horizontal(|ui| {
                     ui.vertical(|ui| {
@@ -537,6 +576,8 @@ impl eframe::App for VaporeApp {
                     });
                 });
             });
+
+            // Display the updated target to rebalance the portfolio within a drop menu
             egui::CollapsingHeader::new("Target").show(ui, |ui| {
                 ui.horizontal(|ui| {
                     ui.vertical(|ui| {
@@ -596,6 +637,8 @@ impl eframe::App for VaporeApp {
                     });
                 });
             });
+
+            // Display the updated purchase/sales to rebalance the portfolio within a drop menu
             egui::CollapsingHeader::new("Purchase").show(ui, |ui| {
                 ui.horizontal(|ui| {
                     ui.vertical(|ui| {
